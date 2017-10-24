1f292efc7d2ee44206344b9d2c9797d628ead20a
Leyla Asan Mane <lasan001@gold.ac.uk>
2017-10-24 14:22:12 +0200

    newest

---
+++ Zee AI Game/Assets/_Completed-Assets/Scripts/Tank/Behaviours.cs
@@ -0,0 +1,193 @@
+using UnityEngine;
+using NPBehave;
+using System.Collections.Generic;
+
+namespace Complete
+{
+    /*
+    Example behaviour trees for the Tank AI.  This is partial definition:
+    the core AI code is defined in TankAI.cs.
+
+    Use this file to specifiy your new behaviour tree.
+     */
+    public partial class TankAI : MonoBehaviour
+    {
+        private Root CreateBehaviourTree() {
+
+            System.Random randomMove = new System.Random();
+            float tankSpeed = randomMove.Next(-1, 1);
+
+            switch (m_Behaviour) {
+
+                case 4:
+                    return SpinBehaviour(-0.05f, 1f);
+                case 5:
+                    return TrackBehaviour();
+                case 0:
+                    return Fun();
+                case 1:
+                    return Deadly(); 
+                case 2:
+                    return Frightened();
+                case 3:
+                    return Unpredictable(tankSpeed);
+                
+
+                default:
+                    return new Root (new Action(()=> Turn(0.1f)));
+            }
+        }
+
+        /* Actions */
+
+        private Node StopTurning() {
+            return new Action(() => Turn(0));
+        }
+
+        private Node RandomFire() {
+            return new Action(() => Fire(UnityEngine.Random.Range(0.0f, 1.0f)));
+        }
+
+
+        /* Example behaviour trees */
+
+        // Constantly spin and fire on the spot 
+        private Root SpinBehaviour(float turn, float shoot) {
+            return new Root(new Sequence(
+                        new Action(() => Turn(turn)),
+                        new Action(() => Fire(shoot))
+                    ));
+        }
+
+        // Turn to face your opponent and fire
+        private Root TrackBehaviour() {
+            return new Root(
+                new Service(0.2f, UpdatePerception,
+                    new Selector(
+                        new BlackboardCondition("targetOffCentre", Operator.IS_SMALLER_OR_EQUAL, 0.1f, Stops.IMMEDIATE_RESTART,
+                            // Stop turning and fire
+                            new Sequence(StopTurning(), new Wait(2f), RandomFire())),
+                            new BlackboardCondition("targetOnRight",  Operator.IS_EQUAL, true, Stops.IMMEDIATE_RESTART,  new Action(() => Turn(0.2f))),
+                            // Turn left toward target
+                            new Action(() => Turn(-0.2f))
+                    )
+                )
+            );
+        }
+
+//---------------------------------------------------------------Fun behaviour ---------------------------------------------------------------
+        private Root Fun()
+        {
+            return new Root(
+                new Service(0.2f, UpdatePerception,
+                    new Selector(
+                            new BlackboardCondition("targetOffCentre", Operator.IS_SMALLER_OR_EQUAL, 0.5f, Stops.IMMEDIATE_RESTART,
+
+                            new Sequence(StopTurning(), new Wait(1f), RandomFire())),
+
+                        new BlackboardCondition("targetOnRight", Operator.IS_EQUAL, true, Stops.IMMEDIATE_RESTART, new Action(() => Turn(0.9f))),
+
+                        new BlackboardCondition("targetOnRight", Operator.IS_EQUAL, false, Stops.IMMEDIATE_RESTART, new Action(() => Turn(0.9f)))
+                                             
+                          
+                    )
+                )
+            );
+        }
+//---------------------------------------------------------------------------------------------------------------------------------------------
+
+//---------------------------------------------------------------Deadly behaviour ----------------------------------------------------------------
+        private Root Deadly()
+        {
+            return new Root(
+                new Service(0.2f, UpdatePerception, new Selector(
+                    
+                             new BlackboardCondition("targetOffCentre", Operator.IS_SMALLER_OR_EQUAL, 0.1f, Stops.IMMEDIATE_RESTART,
+                             //if tank is not facing the enemy then turn
+                             //new Action(() => Turn(0.2f))
+                             new Sequence(StopTurning(), new Wait(1f), RandomFire())
+                             ),
+
+                             new BlackboardCondition("targetOnRight", Operator.IS_EQUAL, true, Stops.IMMEDIATE_RESTART,
+                             //if tank is not facing the enemy then turn
+                             new Action(() => Turn(0.2f))),
+
+                             new BlackboardCondition("targetOnRight", Operator.IS_EQUAL, false, Stops.IMMEDIATE_RESTART,
+                             //if tank is not facing the enemy then turn
+                             new Action(() => Turn(-0.2f))),
+
+                            new BlackboardCondition("targetDistance", Operator.IS_SMALLER_OR_EQUAL, 15f, Stops.IMMEDIATE_RESTART, 
+                            //if the enemy is NOT the distance field then move towards the enemy
+                            new Action(() => Move(0.1f))),
+
+                            new BlackboardCondition("targetInFront", Operator.IS_EQUAL, true, Stops.IMMEDIATE_RESTART,
+                           // LOOK FOR ENEMY, if enemy is in front then stop turning and shoot
+                           // new Sequence(StopTurning(), new Wait(2f), RandomFire())
+                           new Sequence(StopTurning())
+                            )
+                )
+                )
+            );
+        }
+        //---------------------------------------------------------------------------------------------------------------------------------------------
+
+        //---------------------------------------------------------------Frightened behaviour ----------------------------------------------------------------
+        private Root Frightened()
+        {
+            return new Root(
+                new Service(0.2f, UpdatePerception,
+                    new Selector(
+
+                        new BlackboardCondition("targetDistance", Operator.IS_SMALLER_OR_EQUAL, 25f, Stops.IMMEDIATE_RESTART,
+                             new Action(() => Move(-1f))),
+
+                        new BlackboardCondition("targetDistance", Operator.IS_SMALLER_OR_EQUAL, 50f, Stops.IMMEDIATE_RESTART,
+                             new Sequence(StopTurning(), new Wait(0.5f), RandomFire()))
+
+                        
+                           //it will just act scared, shoot and end up being cornered
+                        
+                    )
+                )
+            );
+        }
+        //---------------------------------------------------------------------------------------------------------------------------------------------
+
+        //---------------------------------------------------------------Unpredictable behaviour ----------------------------------------------------------------
+        private Root Unpredictable(float randomMove)
+        {
+            return new Root(
+                new Service(0.2f, UpdatePerception,
+                    new Selector(
+
+                        new BlackboardCondition("targetOnRight", Operator.IS_EQUAL, true, Stops.IMMEDIATE_RESTART,
+
+                            new Action(() => Move(randomMove))),
+
+                            new BlackboardCondition("targetInFront", Operator.IS_EQUAL, true, Stops.IMMEDIATE_RESTART,
+
+                             new Action(() => Fire(1))),
+
+                            new BlackboardCondition("targetInFront", Operator.IS_EQUAL, false, Stops.IMMEDIATE_RESTART,
+
+                             new Action(() => Fire(0)))
+
+                            
+                    )
+                )
+            );
+        }
+        //---------------------------------------------------------------------------------------------------------------------------------------------
+
+        private void UpdatePerception() {
+            Vector3 targetPos = TargetTransform().position;
+            Vector3 localPos = this.transform.InverseTransformPoint(targetPos);
+            Vector3 heading = localPos.normalized;
+            blackboard["targetDistance"] = localPos.magnitude;
+            blackboard["targetInFront"] = heading.z > 0;
+            blackboard["targetOnRight"] = heading.x > 0;
+            blackboard["targetOffCentre"] = Mathf.Abs(heading.x);
+        }
+
+    }
+}
---
+++ Zee AI Game/Assets/_Completed-Assets/Scripts/Tank/TankShooting.cs
@@ -0,0 +1,158 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Complete
+{
+    public class TankShooting : MonoBehaviour
+    {
+        public int m_PlayerNumber = 1;              // Used to identify the different players.
+        public Rigidbody m_Shell;                   // Prefab of the shell.
+        public Transform m_FireTransform;           // A child of the tank where the shells are spawned.
+        public Slider m_AimSlider;                  // A child of the tank that displays the current launch force.
+        public AudioSource m_ShootingAudio;         // Reference to the audio source used to play the shooting audio. NB: different to the movement audio source.
+        public AudioClip m_ChargingClip;            // Audio that plays when each shot is charging up.
+        public AudioClip m_FireClip;                // Audio that plays when each shot is fired.
+        public float m_MinLaunchForce = 15f;        // The force given to the shell if the fire button is not held.
+        public float m_MaxLaunchForce = 30f;        // The force given to the shell if the fire button is held for the max charge time.
+        public float m_MaxChargeTime = 0.75f;       // How long the shell can charge for before it is fired at max force.
+
+        public bool m_NPC;                          // Is shooting AI controlled?
+
+        private string m_FireButton;                // The input axis that is used for launching shells.
+        private float m_CurrentLaunchForce;         // The force that will be given to the shell when the fire button is released.
+        private float m_ChargeSpeed;                // How fast the launch force increases, based on the max charge time.
+
+        private int m_FiringState;  // Current state of the firing sequence
+        private const int FIRING_INACTIVE = 0;
+        private const int FIRING_START = 1;
+        private const int FIRING_CHARGE = 2;
+        private const int FIRING_RELEASE = 3;
+
+        private bool m_AIFireRequest;         // Does the AI want to fire?
+        private float m_AILaunchForce;         // The AI's intended launch force
+
+        private void OnEnable()
+        {
+            // When the tank is turned on, reset the launch force and the UI
+            m_CurrentLaunchForce = m_MinLaunchForce;
+            m_AimSlider.value = m_MinLaunchForce;
+            m_FiringState = FIRING_INACTIVE;
+        }
+
+
+        private void Start ()
+        {
+            // The fire axis is based on the player number.
+            m_FireButton = "Fire" + m_PlayerNumber;
+
+            // The rate that the launch force charges up is the range of possible forces by the max charge time.
+            m_ChargeSpeed = (m_MaxLaunchForce - m_MinLaunchForce) / m_MaxChargeTime;
+        }
+
+
+        private void Update ()
+        {
+            // The slider should have a default value of the minimum launch force.
+            m_AimSlider.value = m_MinLaunchForce;
+
+            // Step forward the firing state machine
+            switch (m_FiringState) {
+
+                case FIRING_INACTIVE:
+
+                    // AI control
+                    if (m_NPC) {
+                        // If AI has requested fire...
+                        if (m_AIFireRequest) {
+                            m_FiringState = FIRING_START;                            
+                        }
+
+                    // Player control
+                    } else {
+                        // If the fire button has just started being pressed...
+                        if (Input.GetButtonDown (m_FireButton)) {
+                            m_FiringState = FIRING_START;
+                        }
+                    }
+                    break;
+
+                case FIRING_START:
+                    // Reset the launch force.
+                    m_CurrentLaunchForce = m_MinLaunchForce;
+
+                    // Change the clip to the charging clip and start it playing.
+                    m_ShootingAudio.clip = m_ChargingClip;
+                    m_ShootingAudio.Play ();
+
+                    // Automatically start charging
+                    m_FiringState = FIRING_CHARGE;
+                    break;
+
+                case FIRING_CHARGE:
+
+                    // Increment the launch force and update the slider.
+                    m_CurrentLaunchForce += m_ChargeSpeed * Time.deltaTime;
+                    m_AimSlider.value = m_CurrentLaunchForce;
+
+                    // If the max force has been exceeded and the shell hasn't yet been launched...
+                    if (m_CurrentLaunchForce >= m_MaxLaunchForce)
+                    {
+                    // ... use the max force and launch the shell.
+                        m_CurrentLaunchForce = m_MaxLaunchForce;
+                        m_FiringState = FIRING_RELEASE;
+                    }
+
+                    // AI control
+                    if (m_NPC) {
+                        if (m_CurrentLaunchForce > m_AILaunchForce) {
+                            m_FiringState = FIRING_RELEASE;
+                        }
+    
+                    // Player control
+                    } else {
+
+                        /// if the fire button is released and the shell hasn't been launched yet...
+                        if (Input.GetButtonUp (m_FireButton)) {
+                            m_FiringState = FIRING_RELEASE;
+                        }
+                    }
+                    break;
+
+                case FIRING_RELEASE:
+                    m_FiringState = FIRING_INACTIVE;
+                    m_AIFireRequest = false;
+                    Fire();
+                    break;
+            
+            }
+        }
+
+        public bool AIFire(float force) {
+            if (!m_AIFireRequest) {
+                m_AIFireRequest = true;
+                force = (force < 0) ? 0 : (force > 1) ? 1 : force;
+                m_AILaunchForce = m_MinLaunchForce + force * (m_MaxLaunchForce - m_MinLaunchForce);
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+        private void Fire ()
+        {
+            // Create an instance of the shell and store a reference to it's rigidbody.
+            Rigidbody shellInstance =
+                Instantiate (m_Shell, m_FireTransform.position, m_FireTransform.rotation) as Rigidbody;
+
+            // Set the shell's velocity to the launch force in the fire position's forward direction.
+            shellInstance.velocity = m_CurrentLaunchForce * m_FireTransform.forward; 
+
+            // Change the clip to the firing clip and play it.
+            m_ShootingAudio.clip = m_FireClip;
+            m_ShootingAudio.Play ();
+
+            // Reset the launch force.  This is a precaution in case of missing button events.
+            m_CurrentLaunchForce = m_MinLaunchForce;
+        }
+    }
+}
